c0bf730574808b7e0aeee3d6cea17fb08972381d
WizuO <wizu@musiqcity.com>
2015-06-18 10:07:04 -0400

    Added swipe menu

---
+++ Commons Menu1/Commons Menu1/ToDoItem.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoItem.swift
+//  Commons Menu1
+//
+//  Created by Bjorn Ordoubadian on 18/6/15.
+//  Copyright (c) 2015 Davidson College Mobile App Team. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItem: NSObject {
+   
+    // A text description of this item.
+    var text: String
+    
+    // A Boolean value that determines the completed state of this item.
+    var completed: Bool
+    
+    // Returns a ToDoItem initialized with the given text and default completed value.
+    init(text: String) {
+        self.text = text
+        self.completed = false
+    }
+
+    
+}
