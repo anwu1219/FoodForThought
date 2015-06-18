b765ff4b40709cb1c8c1a4f592528f9a2ec00497
Andrew Wu <anwu@andrews-mbp.wireless.davidson.edu>
2015-06-18 09:54:20 -0400

    swipe table

---
+++ Commons Menu1/Commons Menu1/Dish.swift
@@ -0,0 +1,24 @@
+//
+//  Dish.swift
+//  Commons Menu1
+//
+//  Created by Andrew Wu on 6/18/15.
+//  Copyright (c) 2015 Davidson College Mobile App Team. All rights reserved.
+//
+
+import UIKit
+
+class Dish: NSObject {
+    // A text description of this item.
+    var name: String
+    
+    // A Boolean value that determines the completed state of this item.
+    var completed: Bool
+    
+    // Returns a ToDoItem initialized with the given text and default completed value.
+    init(text: String) {
+        self.name = text
+        self.completed = false
+    }
+
+}
---
+++ Commons Menu1/Commons Menu1/TableViewCell.swift
@@ -0,0 +1,99 @@
+//
+//  TableViewCell.swift
+//  Commons Menu1
+//
+//  Created by Andrew Wu on 6/18/15.
+//  Copyright (c) 2015 Davidson College Mobile App Team. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+// A protocol that the TableViewCell uses to inform its delegate of state change
+protocol TableViewCellDelegate {
+    // indicates that the given item has been deleted
+    func dishDeleted(dish: Dish)
+}
+
+class TableViewCell: UITableViewCell {
+    
+    // The object that acts as delegate for this cell.
+    var delegate: TableViewCellDelegate?
+    // The item that this cell renders.
+    var dish: Dish?
+    let gradientLayer = CAGradientLayer()
+    var originalCenter = CGPoint()
+    var deleteOnDragRelease = false
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("NSCoding not supported")
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        // gradient layer for cell
+        gradientLayer.frame = bounds
+        let color1 = UIColor(white: 1.0, alpha: 0.2).CGColor as CGColorRef
+        let color2 = UIColor(white: 1.0, alpha: 0.1).CGColor as CGColorRef
+        let color3 = UIColor.clearColor().CGColor as CGColorRef
+        let color4 = UIColor(white: 0.0, alpha: 0.1).CGColor as CGColorRef
+        gradientLayer.colors = [color1, color2, color3, color4]
+        gradientLayer.locations = [0.0, 0.01, 0.95, 1.0]
+        layer.insertSublayer(gradientLayer, atIndex: 0)
+        // add a pan recognizer
+        var recognizer = UIPanGestureRecognizer(target: self, action: "handlePan:")
+        recognizer.delegate = self
+        addGestureRecognizer(recognizer)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+    }
+    
+    
+    //MARK: - horizontal pan gesture methods
+    func handlePan(recognizer: UIPanGestureRecognizer) {
+        // 1
+        if recognizer.state == .Began {
+            // when the gesture begins, record the current center location
+            originalCenter = center
+        }
+        // 2
+        if recognizer.state == .Changed {
+            let translation = recognizer.translationInView(self)
+            center = CGPointMake(originalCenter.x + translation.x, originalCenter.y)
+            // has the user dragged the item far enough to initiate a delete/complete?
+            deleteOnDragRelease = frame.origin.x < -frame.size.width / 2.0
+        }
+        // 3
+        if recognizer.state == .Ended {
+            // the frame this cell had before user dragged it
+            let originalFrame = CGRect(x: 0, y: frame.origin.y,
+                width: bounds.size.width, height: bounds.size.height)
+            if !deleteOnDragRelease {
+                // if the item is not being deleted, snap back to the original location
+                UIView.animateWithDuration(0.2, animations: {self.frame = originalFrame})
+            }
+            if deleteOnDragRelease {
+                if delegate != nil && dish != nil {
+                    // notify the delegate that this item should be deleted
+                    delegate!.dishDeleted(dish!)
+                }
+            }
+        }
+    }
+    
+    override func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
+        if let panGestureRecognizer = gestureRecognizer as? UIPanGestureRecognizer {
+            let translation = panGestureRecognizer.translationInView(superview!)
+            if fabs(translation.x) > fabs(translation.y) {
+                return true
+            }
+            return false
+        }
+        return false
+    }
+    
+}
