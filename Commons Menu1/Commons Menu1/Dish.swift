93d11bff866c8537fe3d72eeb364fcd519964cb0
anwu1219 <anwu@andrews-mbp.wireless.davidson.edu>
2015-06-18 10:51:01 -0400

    Update Variable Names

--- Commons Menu1/Commons Menu1/Dish.swift
+++ Commons Menu1/Commons Menu1/Dish.swift
@@ -1,23 +1,22 @@
 //
-//  ToDoItem.swift
+//  Dish.swift
 //  Commons Menu1
 //
-//  Created by Bjorn Ordoubadian on 18/6/15.
+//  Created by Andrew Wu on 6/18/15.
 //  Copyright (c) 2015 Davidson College Mobile App Team. All rights reserved.
 //
-
 import UIKit
 
-class ToDoItem: NSObject {
+class Dish: NSObject {
     // A text description of this item.
-    var text: String
+    var name: String
     
     // A Boolean value that determines the completed state of this item.
     var completed: Bool
     
     // Returns a ToDoItem initialized with the given text and default completed value.
     init(text: String) {
-        self.text = text
+        self.name = text
         self.completed = false
     }
 }
