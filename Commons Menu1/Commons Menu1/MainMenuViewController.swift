22b017609e64ab9ec33045a52b3b25fb5097453f
Wu <anwu@w11387-3.wireless.davidson.edu>
2015-06-26 12:15:03 -0400

    User Sign up / log in

--- Commons Menu1/Commons Menu1/MainMenuViewController.swift
+++ Commons Menu1/Commons Menu1/MainMenuViewController.swift
@@ -21,22 +21,88 @@ class MainMenuViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let user = PFUser()
-        user.username = "my name"
-        user.password = "my pass"
-        user.email = "email@example.com"
-        
-        // other fields can be set if you want to save more information
-        user["phone"] = "650-555-0000"
-        
-        user.signUpInBackgroundWithBlock { (succeeded, error) -> Void in
-            if error == nil {
-                println("success")
-            } else {
-                println("\(error)");
-        // Do any additional setup after loading the view, typically from a nib.
-            }
+        if PFUser.currentUser() == nil{
+            var loginAlert:UIAlertController = UIAlertController(title: "Sign Up / Login", message: "Please sign up or login", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            loginAlert.addTextFieldWithConfigurationHandler({
+                textfield in
+                textfield.placeholder = "Your username"
+            })
+            
+            loginAlert.addTextFieldWithConfigurationHandler({
+                textfield in
+                textfield.placeholder = "Your password"
+                textfield.secureTextEntry = true
+            })
+            
+            loginAlert.addAction(UIAlertAction(title: "Login", style: UIAlertActionStyle.Default, handler: {
+                alertAction in
+                let textFields:NSArray = loginAlert.textFields as! NSArray
+                let usernameTextfield:UITextField = textFields.objectAtIndex(0) as! UITextField
+                let passwordTextfield:UITextField = textFields.objectAtIndex(1) as! UITextField
+                
+                if let usernameTextField = textFields.objectAtIndex(0) as? UITextField{
+                    if let passwordTextField = textFields.objectAtIndex(1) as? UITextField{
+                        if let user = usernameTextField.text{
+                            if let password = passwordTextField.text{
+                                                    PFUser.logInWithUsername(user, password: password)
+                    }
+                }
+                    }
+                }
+              
+                
+                
+                
+                
+            }))
+            
+            loginAlert.addAction(UIAlertAction(title: "Sign Up", style: UIAlertActionStyle.Default, handler: {
+                alertAction in
+                let textFields:NSArray = loginAlert.textFields as! NSArray
+                let usernameTextfield:UITextField = textFields.objectAtIndex(0) as! UITextField
+                let passwordTextfield:UITextField = textFields.objectAtIndex(1) as! UITextField
+                
+                var user:PFUser = PFUser()
+                user.username = usernameTextfield.text
+                user.password = passwordTextfield.text
+                
+                user.signUpInBackgroundWithBlock{
+                    (success: Bool, error:NSError?)->Void in
+                    if error == nil{
+                        println("Sign Up successfull")
+                    }else{
+                        let errorString = error!.localizedDescription
+                        println(errorString)
+                    }
+                    
+                    
+                }
+                
+                
+                
+            }))
+            
+            self.presentViewController(loginAlert, animated: true, completion: nil)
+        } else {
+            println("logged in")
         }
+//        let user = PFUser()
+//        user.username = "my name"
+//        user.password = "my pass"
+//        user.email = "email@example.com"
+//        
+//        // other fields can be set if you want to save more information
+//        user["phone"] = "650-555-0000"
+//        
+//        user.signUpInBackgroundWithBlock { (succeeded, error) -> Void in
+//            if error == nil {
+//                println("success")
+//            } else {
+//                println("\(error)");
+//        // Do any additional setup after loading the view, typically from a nib.
+//            }
+//        }
         
         menuButton.backgroundColor = styles.buttonBackgoundColor
         menuButton.layer.cornerRadius = styles.buttonCornerRadius
