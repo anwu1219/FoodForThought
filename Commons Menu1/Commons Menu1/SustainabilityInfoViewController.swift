2e5510c202a5eb045f523a7f7c7cf0f153bf5a19
WizuO <wizu@musiqcity.com>
2015-06-29 09:55:34 -0400

    linked learn more buttons to sustainability website

--- Commons Menu1/Commons Menu1/SustainabilityInfoViewController.swift
+++ Commons Menu1/Commons Menu1/SustainabilityInfoViewController.swift
@@ -17,6 +17,18 @@ class SustainabilityInfoViewController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
 
+    @IBAction func learnMoreAction(sender: AnyObject) {
+        UIApplication.sharedApplication().openURL(NSURL(string: "http://sites.davidson.edu/sustainabilityscholars/")!)
+    }
+    
+    @IBAction func learnMoreAction2(sender: AnyObject) {
+        UIApplication.sharedApplication().openURL(NSURL(string: "http://sites.davidson.edu/sustainabilityscholars/")!)
+    }
+    
+    @IBAction func learnMoreAction3(sender: AnyObject) {
+        UIApplication.sharedApplication().openURL(NSURL(string: "http://sites.davidson.edu/sustainabilityscholars/")!)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
