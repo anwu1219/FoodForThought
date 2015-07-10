661b57f0beec11904b9cc91c42a60c59a83f7aa0
Wu <anwu@w11387-2.wireless.davidson.edu>
2015-07-09 23:45:39 -0400

    Default image setting

--- Commons Menu1/Commons Menu1/RestMenuViewController.swift
+++ Commons Menu1/Commons Menu1/RestMenuViewController.swift
@@ -113,22 +113,20 @@ class RestMenuViewController: UIViewController {
             let button = sender as! UIButton
             if let title = button.titleLabel?.text {
                 for restaurant : RestProfile in restaurants.keys{
-                    if loaded[find(keys, restaurant)!] == false {
                         if restaurant.name == title{
-                            self.addDishWithLocation(restaurant.name)
-                            loaded[find(keys, restaurant)!] = true
-                            presentViewController(loadingAlert, animated: true, completion: nil)
-                            let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(NSEC_PER_SEC * 1))
-                            dispatch_after(delayTime, dispatch_get_main_queue()){
-                            self.performSegueWithIdentifier("restToMenuSegue", sender: sender)
+                            if loaded[find(keys, restaurant)!] == false {
+                                self.addDishWithLocation(restaurant.name)
+                                loaded[find(keys, restaurant)!] = true
+                                presentViewController(loadingAlert, animated: true, completion: nil)
+                                let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(NSEC_PER_SEC * 1))
+                                dispatch_after(delayTime, dispatch_get_main_queue()){
+                                self.loadingAlert.dismissViewControllerAnimated(true, completion: { () -> Void in
+                                    self.performSegueWithIdentifier("restToMenuSegue", sender: sender)
+                                })
                             }
-                            self.loadingAlert.dismissViewControllerAnimated(true, completion: { () -> Void in
-                            
-                            })
                         }
-                    } else {
-                         self.performSegueWithIdentifier("restToMenuSegue", sender: sender)
-                    }
+                        self.performSegueWithIdentifier("restToMenuSegue", sender: sender)
+                    } 
                 }
             }
         }
@@ -155,25 +153,25 @@ class RestMenuViewController: UIViewController {
                                                                         if let ingredients = object["ingredients"] as? [String]{
                                                                                 if let labels = object["labels"] as? [[String]]{
                                                                                         if let type = object["type"] as? String{
-                                                                                                let dish = Dish(name: name, image: image, location: location, type: type, ingredients: ingredients, labels: labels)
-                                                            
-                                                                                                self.dishes.addDish(location, dish: dish)
-                                                                                        }
+                                                                                            let dish = Dish(name: name, image: image, location: location, type: type, ingredients: ingredients, labels: labels)
+                                                                                            self.dishes.addDish(location, dish: dish)
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
                                                         }
+                                                }
+                                        } else
+                                        {
+                                        if let location = object["location"] as? String{
+                                            if let ingredients = object["ingredients"] as? [String]{
+                                                if let labels = object["labels"] as? [[String]]{
+                                                    if let type = object["type"] as? String{
+                                                        let dish = Dish(name: name, location: location, type: type, ingredients: ingredients, labels: labels)
+                                                        self.dishes.addDish(location, dish: dish)
                                                     }
                                                 }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }else {
-                                if let location = object["location"] as? String{
-                                    if let ingredients = object["ingredients"] as? [String]{
-                                        if let labels = object["labels"] as? [[String]]{
-                                            if let type = object["type"] as? String{
-                                                let dish = Dish(name: name, location: location, type: type, ingredients: ingredients, labels: labels)
-                                                self.dishes.addDish(location, dish: dish)
                                             }
                                         }
                                     }
