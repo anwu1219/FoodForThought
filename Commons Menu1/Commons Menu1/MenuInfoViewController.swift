a791db1d1c79ce8be992f8292cfcc97e13c560ad
Chadinha <spchadinha@w11410-3.employeewireless.davidson.edu>
2015-07-10 14:28:17 -0400

    getting ready

--- Commons Menu1/Commons Menu1/MenuInfoViewController.swift
+++ Commons Menu1/Commons Menu1/MenuInfoViewController.swift
@@ -22,19 +22,20 @@ class MealInfoViewController: UIViewController {
     @IBOutlet weak var labelsLabel: UILabel!
     
     var dish: Dish?
-    
+    let screenSize: CGRect = UIScreen.mainScreen().bounds
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         dishName.title = dish?.name
         dishImage.image = dish?.image
+        dishImage.frame = CGRectMake(0, 0, screenSize.width, screenSize.height*0.4)
         scrollInfo.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 0.8)
-        var scrollViewHeight = 350
+        var scrollViewHeight = screenSize.height * 0.6
         if let ingredient = dish?.ingredients{
-            scrollViewHeight += ingredient.count * 30
+            scrollViewHeight += CGFloat(ingredient.count)
         }
-        scrollInfo.contentSize.height = CGFloat(scrollViewHeight)
+        scrollInfo.contentSize.height = scrollViewHeight
         scrollInfo.layer.borderColor = UIColor.greenColor().CGColor
         scrollInfo.layer.borderWidth = 2
         layoutPage()
@@ -46,24 +47,24 @@ class MealInfoViewController: UIViewController {
     Put dish information into the scrollInfo
     */
     func layoutPage() {
-        var width: CGFloat = 0.01 * scrollInfo.bounds.width // a unit of x
-        var height: CGFloat = 0.01 * scrollInfo.bounds.height // a unit of y
-        var x: CGFloat = 0.02 * scrollInfo.bounds.width // current x coordinate
-        var y: CGFloat = 0.05 * scrollInfo.bounds.height // current y coordinate
+        var width: CGFloat = 0.01 * screenSize.width // a unit of x
+        var height: CGFloat = 0.01 * screenSize.height // a unit of y
+        var x: CGFloat = 0.02 * screenSize.width // current x coordinate
+        var y: CGFloat = 0.01 * screenSize.height // current y coordinate
         
         if let susLabels = dish?.labels {
             if susLabels.count > 0 {
                 var title = UILabel()
                 title.text = "Dish Sustainability Info \(susLabels.count)"
-                title.frame = CGRectMake(x, y, 57*width, 20*height)
+                title.frame = CGRectMake(x, y, 91*width, 10*height)
                 title.textAlignment = .Center
                 title.backgroundColor = UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1.0)
                 scrollInfo.addSubview(title)
-                y += 20 * height
+                y += 10 * height
                 
                 var labelPics = UIScrollView()
-                var labelWidth = 18*height
-                var labelHeight = 18*height
+                var labelWidth = 9*height
+                var labelHeight = 9*height
                 var labelSpace = 2*width
                 labelPics.frame = CGRectMake(x, y, title.frame.width, title.frame.height)
                 labelPics.contentSize = CGSizeMake(CGFloat(susLabels.count)*(labelWidth+labelSpace)+labelSpace, labelHeight)
@@ -85,15 +86,15 @@ class MealInfoViewController: UIViewController {
                 }
                 
                 scrollInfo.addSubview(labelPics)
-                y += 20 * height
+                y += 10 * height
             }
         }
         
         if let location = dish?.location {
             var label = UILabel()
             label.text = "Location: \(location)"
-            label.frame = CGRectMake( x, y, 20 * width, 10 * height)
-            label.backgroundColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 0.8)
+            label.frame = CGRectMake( x, y, 91 * width, 10 * height)
+            //label.backgroundColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 0.8)
             scrollInfo.addSubview(label)
             y += 10 * height
         }
@@ -101,7 +102,7 @@ class MealInfoViewController: UIViewController {
         if let price = dish?.price {
             var label = UILabel()
             label.text = "Price: $\(price)"
-            label.frame = CGRectMake(x, y, 20 * width, 10 * height)
+            label.frame = CGRectMake(x, y, 91 * width, 10 * height)
             label.backgroundColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 0.8)
             scrollInfo.addSubview(label)
             y += 10 * height
@@ -112,9 +113,9 @@ class MealInfoViewController: UIViewController {
             //label.numberOfLines = 0 // allows for undetermined number of lines to be used to display text
             var ingreident = UILabel()
             ingreident.text = "Ingredients:"
-            ingreident.frame = CGRectMake(x, y, 20 * width, 10 * height)
+            ingreident.frame = CGRectMake(x, y, 91 * width, 10 * height)
             scrollInfo.addSubview(ingreident)
-            y += 12 * height
+            y += 9 * height
             for var i = 0; i < ingredients.count; i++ {
                 var ingredient = UILabel()
                 ingredient.text = "- \(ingredients[i])"
@@ -127,17 +128,17 @@ class MealInfoViewController: UIViewController {
 
                 if let labels = dish?.labels {
                     if !labels.isEmpty{
-                        var nutLabelXPosition = 10 * height
+                        var nutLabelXPosition = 3 * height
                         for label: String in labels[i] {
-                            var nutLabel = UIImageView(frame: CGRectMake( 330 - nutLabelXPosition, y - 2 * height, 10 * height, 10 * height))
+                            var nutLabel = UIImageView(frame: CGRectMake( 330 - nutLabelXPosition, y - 2 * height, 6 * height, 6 * height))
                             var image = UIImage(named: label)
                             nutLabel.image = image
                             scrollInfo.addSubview(nutLabel)
-                            nutLabelXPosition += 11 * height
+                            nutLabelXPosition += 8 * height
                         }
                     }
                 }
-                y += 10 * height
+                y += 7 * height
             }
         }
     }
