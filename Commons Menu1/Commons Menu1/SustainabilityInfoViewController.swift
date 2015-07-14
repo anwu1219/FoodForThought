08719296363d0f2be9af64cd3c335d384ad71f65
WizuO <wizu@musiqcity.com>
2015-07-14 14:19:34 -0400

    some small UI changes

--- Commons Menu1/Commons Menu1/SustainabilityInfoViewController.swift
+++ Commons Menu1/Commons Menu1/SustainabilityInfoViewController.swift
@@ -29,22 +29,23 @@ class SustainabilityInfoViewController: UIViewController {
         let verticalSpace = 0.05 * screenSize.height
         let widthPadding = 0.05 * screenSize.width
         var y: CGFloat = 0.0
-        susView.backgroundColor = UIColor(red: 0.6, green: 0.78, blue: 0.6, alpha: 1.0)
-        susView.layer.borderColor = UIColor(red: 0.1, green: 0.35, blue: 0.1, alpha: 1.0).CGColor
+        susView.backgroundColor = UIColor(red: 243/255.0, green: 244/255.0, blue: 230/255.0, alpha: 1)
+        susView.layer.borderColor = UIColor(red: 64/255.0, green: 55/255.0, blue: 74/255.0, alpha: 0.95).CGColor
         susView.layer.borderWidth = 10
         susView.layer.cornerRadius = 5
 
         
         super.viewDidLoad()
         
-        susView.frame = CGRectMake(widthPadding, 3*verticalSpace, screenSize.width-(2*widthPadding), screenSize.height - (4*verticalSpace))
+        susView.frame = CGRectMake(widthPadding, 3.5*verticalSpace, screenSize.width-(2*widthPadding), screenSize.height - (4*verticalSpace))
         let background = UIImageView()
-        background.image = UIImage(named: "wheat")
+        background.image = UIImage(named: "SustyPageBackground")
         background.frame = CGRectMake(0.0, 0.0, screenSize.width, screenSize.height) // will need to change with new images
         background.bounds = background.bounds
         background.contentMode = .ScaleToFill
         self.view.addSubview(background)
         //self.view.sendSubviewToBack(background)
+
         
         for var i = 0; i < 3; i++ {
             let container = UIView()
@@ -77,7 +78,8 @@ class SustainabilityInfoViewController: UIViewController {
             
             // set the subsection's body text
             body.text = susInfo[i]
-            body.textColor = UIColor.whiteColor()
+            body.font = UIFont(name: "HelveticaNeue-Light", size: 14)
+            body.textColor = UIColor(red: 64/255.0, green: 55/255.0, blue: 74/255.0, alpha: 0.95)
             body.lineBreakMode = NSLineBreakMode.ByWordWrapping
             body.numberOfLines = 0
             body.frame = CGRectMake(0.0, y, 1000, 1000)
