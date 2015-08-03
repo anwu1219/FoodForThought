89b2cfe5fed949d16da803b354d87b777884cf63
Wu <anwu@w11387.local>
2015-08-03 12:32:27 -0400

    Table Cell

---
+++ Commons Menu1/Commons Menu1/ExpandTableViewCell.swift
@@ -0,0 +1,63 @@
+//
+//  ExpandTableViewCell.swift
+//  Foodscape
+//
+//  Created by Wu, Andrew on 8/3/15.
+//  Copyright (c) 2015 Davidson College. All rights reserved.
+//
+
+import UIKit
+
+class ExpandTableViewCell: UITableViewCell {
+    let titleLabel : UILabel
+    let contentLabel : UILabel
+    class var expandedHeight: CGFloat { get { return 200 } }
+    class var defaultHeight: CGFloat  { get { return 44  } }
+    
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("NSCoding not supported")
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        titleLabel = UILabel(frame: CGRect.nullRect)
+        titleLabel.textColor = UIColor.blackColor()
+        titleLabel.numberOfLines = 0
+        titleLabel.lineBreakMode = NSLineBreakMode.ByWordWrapping
+
+        
+        contentLabel = UILabel(frame: CGRect.nullRect)
+        contentLabel.textColor = UIColor.blackColor()
+        contentLabel.numberOfLines = 0
+        contentLabel.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubview(titleLabel)
+        addSubview(contentLabel)
+        
+        titleLabel.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
+        contentLabel.frame = CGRect(x:0, y:self.frame.height, width:self.frame.width, height:100)
+        contentLabel.sizeToFit()
+        contentLabel.frame.size.width = self.frame.width
+    }
+    
+    
+    func checkHeight() {
+        contentLabel.hidden = (frame.size.height < ExpandTableViewCell.expandedHeight)
+    }
+        
+    func watchFrameChanges() {
+        addObserver(self, forKeyPath: "frame", options: NSKeyValueObservingOptions.New|NSKeyValueObservingOptions.Initial, context: nil)
+    }
+        
+    func ignoreFrameChanges() {
+        removeObserver(self, forKeyPath: "frame")
+    }
+    
+    
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change:[NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        if keyPath == "frame" {
+            checkHeight()
+        }
+    }
+}
--- Commons Menu1/Commons Menu1/SustainabilityInfoViewController.swift
+++ Commons Menu1/Commons Menu1/SustainabilityInfoViewController.swift
@@ -17,8 +17,9 @@ Displays information of sustainability and links to sustainability info
 */
 class SustainabilityInfoViewController: UIViewController, UIPopoverPresentationControllerDelegate, UITableViewDataSource, UITableViewDelegate, TypesTableViewCellDelegate, UIScrollViewDelegate{
     
-    
+    var selectedIndexPath : NSIndexPath?
     var isFromInfo : Bool?
+    let defaultHeight = UIScreen.mainScreen().bounds.height * 0.1
     private var labelPositions = [String: CGFloat]()
     private let menuSwipeScroll = UIScrollView()
     private let  typesTableView = UITableView()
@@ -27,8 +28,10 @@ class SustainabilityInfoViewController: UIViewController, UIPopoverPresentationC
     private let susView = UIScrollView()
     private let icon = UIImageView()
     private let sustainabilityImages = ["greenEarth", "heartHands", "treeCoin"]
-    private let susLabels = ["About Sustainability", "Sustainability Labels"]
-    private let susInfo = ["Our sustainability team seeks to promote sustainability among local communities by providing consumers with relevant food-related sustainability information within a useful meal planning tool. This tool integrates the triple bottom line (equity, environment, economy) into our culture in order to encourage sustainable communities, businesses and lifestyles. \n Sustainability must include the inextricable links among equity, environment and economy (the three E’s). The Great Law of the Iroquois and the definition of sustainable development in the Brundtland Commission Report of 1987 best exemplify the concept of sustainability: \n “In every deliberation, we must consider the impact on the seventh generation” \n The Great Law of the Iroquois\n“Sustainable development is development that meets the needs of the present without compromising the ability of future generations to meet their own needs”\n Brundtland Commission \n We use the triple bottom line concept coined by John Elkington in his 1994 book, “Cannibals with Forks.” It is a framework to facilitate decision-making because every decision we make affects social equity, environmental integrity and economic prosperity. Improving all three can drive opportunity.\n Some examples of sustainability topics include (but are not limited to):\n Food Justice\n Socially and environmentally-conscious businesses\n Fair labor\n Land use\n Waste management\n Resource consumption\n Healthy living. What you can do:\n Never underestimate the impact a single individual can have on the greater sustainability movement. Simple actions, such as asking restaurant managers questions about their restaurant could motivate them to learn more about sustainability.  Here are some suggestions:\nAsk if the restaurant sources local food.\nSee if you can determine what farms the food you buy comes from.\n Visit your local farmers market. You’ll get fresh food and support the local economy.", "Sustainability must include the inextricable links among equity, environment and economy (the three E’s). The Great Law of the Iroquois and the definition of sustainable development in the Brundtland Commission Report of 1987 best exemplify the concept of sustainability.", "Never underestimate the impact a single individual can have on the greater sustainability movement. Simple actions, such as asking restaurant managers questions about their restaurant could motivate them to learn more about sustainability."]
+    private let susLabels = ["About Sustainability","Sustainability Labels"]
+    private let subTitles = ["Foodscape Purpose", "Foodscape Mission", "Sustainability Defined", "More Than This App: Food Justice, Sports and More", "Examples of Food and Sustainability Topics", "The Davidson College Food System", "Feedback & Suggestions"]
+    private let susInfo = ["Foodscape seeks to promote sustainability among local communities by providing consumers with relevant food-related sustainability information.\nThis app connects the three E's of equity, environment, economy into our local food preferences in order to encourage sustainable communities, businesses and lifestyles. Foodscape predominantly highlights social and environmental aspects of food and locations as self-reported by local restaurants and cafes. Through this mobile app you will learn whether restaurants report sourcing locally, get involved in their communities, source products that believe in fair labor and fair wages, recycle, and much more. See methodology for more about how this information is reported and what it means.\nWhile Foodscape concentrates on the interaction between consumers and the restaurants where they purchase food, the goal is to get everyone to think more about their entire food system. You can see a diagram of the Davidson College food system below. Using this, we can start connecting the dots between seed and fork for all food consumers in Davidson. We hope this helps you understand what questions you want to ask about your food, where it comes from, and your preferences!", "We believe it is important for consumers to reconnect with their food and, in addition to the financial implications of their decisions, understand the social and environmental implications of food production, provision and consumption. We hope this app helps you learn and facilitates discussion about what questions you want to ask yourself and your food providers.\nOur food system includes issues related to health, obesity in the United States, water consumption, environmental health impacts of pesticides and fertilizers, inequities of access and availability to healthy food based on socio-economic differences, economic opportunity for local farmers and restaurants, power struggles within food industries between large corporations and small businesses, identity, culture, diversity, and much more. All of these issues can be connected to impacts of policy, subsidies, consumer behavior, advertising and more. It can be overwhelming. We hope to help create a starting point for your journey to learn more about your food and how it is connected to every part of your life. To find out more about all these topics and more visit food.davidsonsustainability.org.\nAs one of the schools supported by The Duke Endowment, who provided the funding for this initiative, we also want this app to be replicable for other colleges and universities. If your school is interested please contact us at food.davidsonsustainability.org to see how we can help integrate new locations into the app or help your school create its own version.", "Sustainability is a framework that includes the inextricable links among social equity, environmental integrity and economic prosperity – often referred to as the Triple Bottom Line (coined by John Elkington in his 1994 book, Cannibals with Forks). Triple Bottom Line sustainability is a framework we use to facilitate decision-making; because every decision we make affects social equity, environmental integrity and economic prosperity. Improving all three can drive opportunity.\n\n{insert triangle image here}\n\nThe Great Law of the Iroquois and the definition of sustainable development in the Brundtland Commission Report of 1987 best exemplify the concept of sustainability.\n\"In every deliberation, we must consider the impact on the seventh generation.\"\n- The Great Law of the Iroquois\n\"Sustainable development is development that meets the needs of the present without compromising the ability of future generations to meet their own needs.\"\n- Brundtland Commission", "Food and sustainability addresses where our food originates (e.g., local and small scale vs. large scale), how it is grown (organic, biologique (?), naturally grown, etc.), fair labor practices, environmental impact, availability and access to healthy foods, other food justice issues, and much more. Want to learn more about what all this means? Explore all these issues and more at food.davidsonsustainability.org.\nWe know that our mobile app does not touch on every aspect of sustainability and food. However, we are using the website above to pull together more resources from around the globe and from our own students, faculty and staff at Davidson College.\nFood Justice\nOur website provides more detail, information and opportunities related to Food Justice issues.\n\"Food justice asserts that no one should live without enough food because of economic constraints or social inequalities… The food justice movement is a different approach to a community’s needs that seeks to truly advance self-reliance and social justice by placing communities in leadership of their own solutions and providing them with the tools to address the disparities within our food systems and within society at large.\" (Ahmadi, 2010)\nThe United States Department of Agriculture has created some definitions and analysis about Food Security and Insecurity. Here are their definitions (http://www.ers.usda.gov/topics/food-nutrition-assistance/food-security-in-the-us/definitions-of-food-security.aspx)\n\nFood Security\n\u{2022} High food security (old label=Food security): no reported indications of food-access problems or limitations.\n\u{2022} Marginal food security (old label=Food security): one or two reported indications—typically of anxiety over food sufficiency or shortage of food in the house. Little or no indication of changes in diets or food intake.\nFood Insecurity\nu{2022} Low food security (old label=Food insecurity without hunger): reports of reduced quality, variety, or desirability of diet. Little or no indication of reduced food intake.\nu{2022} Very low food security (old label=Food insecurity with hunger): Reports of multiple indications of disrupted eating patterns and reduced food intake.\n\nSports\nFood and sports has become a hot topic. People come together for sports and food. So there is a big opportunity to influence change in healthy eating, local sourcing and organically grown foods. The Green Sports Alliance and Natural Resources Defense Council created a report titled, Champions of Game Day Food. (http://www.nrdc.org/greenbusiness/guides/sports/files/game-day-food-report.pdf). If you want to learn more about how athletes are becoming leaders for food and sustainability, whether hormone and antibiotic free beef or nutritional and vegan diets for athletes, visit food.davidsonsustainability.org.\n","Some examples of sustainability topics in food include (but are not limited to):\nCulture and food\nFair labor\nFood & Identity\nFood justice\nGlobal food issues\nHealthy living\nHormone and antibiotic free meat\nLand use\nLocal sourcing\nPower and Policy\nResource consumption\nSafe and healthy workplace\nSocially and environmentally-conscious businesses\nVeganism\nVegetarianism\nWater use\nWaste management\n\nLearn about some of these examples at food.davidsonsustainability.org", "A Image", "Please provide us with your feedback and suggestions for the Davidson College Office of Sustainability and our partners through our website: food.davidsonsustainability.org."]
+    private let susInfoTableView = UITableView()
     
 
     @IBAction func learnMoreAction(sender: UIButton!) {
@@ -72,7 +75,9 @@ class SustainabilityInfoViewController: UIViewController, UIPopoverPresentationC
         menuSwipeScroll.scrollEnabled = false
         view.addSubview(menuSwipeScroll)
 
-        addScrollView()
+        //addScrollView()
+        
+        addTableView()
         
         
         addScrollIcon()
@@ -82,6 +87,10 @@ class SustainabilityInfoViewController: UIViewController, UIPopoverPresentationC
         typesTableView.registerClass(TypesTableViewCell.self, forCellReuseIdentifier: "typeCell")
         typesTableView.separatorStyle = .None
         
+        susInfoTableView.dataSource = self
+        susInfoTableView.delegate = self
+        susInfoTableView.registerClass(ExpandTableViewCell.self, forCellReuseIdentifier: "susCell")
+        susInfoTableView.separatorStyle = .None
         
     }
     
@@ -250,7 +259,15 @@ class SustainabilityInfoViewController: UIViewController, UIPopoverPresentationC
         }
         }
         susView.contentSize.height = y + verticalSpace
-        menuSwipeScroll.addSubview(susView)
+//        menuSwipeScroll.addSubview(susView)
+    }
+    
+    
+    func addTableView(){
+        let xUnit : CGFloat = self.view.frame.width / 100
+        let yUnit : CGFloat = self.view.frame.height / 100
+        susInfoTableView.frame = CGRect(x: 66 * xUnit, y: 0, width: menuSwipeScroll.frame.width, height: menuSwipeScroll.frame.height)
+        menuSwipeScroll.addSubview(susInfoTableView)
     }
     
     
@@ -278,6 +295,9 @@ class SustainabilityInfoViewController: UIViewController, UIPopoverPresentationC
     
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView == susInfoTableView {
+            return subTitles.count
+        }
         return 3
     }
     
@@ -286,6 +306,14 @@ class SustainabilityInfoViewController: UIViewController, UIPopoverPresentationC
     Generates cells and adds items to the table
     */
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if tableView == susInfoTableView {
+            var cell = tableView.dequeueReusableCellWithIdentifier("susCell", forIndexPath: indexPath) as! ExpandTableViewCell
+            
+            cell.titleLabel.text = self.subTitles[indexPath.row]
+            cell.contentLabel.text = self.subTitles[indexPath.row]
+            cell.backgroundColor = UIColor(white: 0.667, alpha: 0.2)
+            return cell
+        } else {
         
         var cell = tableView.dequeueReusableCellWithIdentifier("typeCell", forIndexPath: indexPath) as! TypesTableViewCell
         if indexPath.row == 0 {
@@ -307,6 +335,54 @@ class SustainabilityInfoViewController: UIViewController, UIPopoverPresentationC
         cell.textLabel!.textColor = UIColor.whiteColor()
         cell.backgroundColor = UIColor(white: 0.667, alpha: 0.2)
         return cell
+        }
+    }
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let previousIndexPath = selectedIndexPath
+        if indexPath == selectedIndexPath {
+            selectedIndexPath = nil
+        } else {
+            selectedIndexPath = indexPath
+        }
+        
+        var indexPaths : Array<NSIndexPath> = []
+        if let previous = previousIndexPath {
+            indexPaths += [previous]
+        }
+        if let current = selectedIndexPath {
+            indexPaths += [current]
+        }
+        if indexPaths.count > 0 {
+            tableView.reloadRowsAtIndexPaths(indexPaths, withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
+    }
+    
+    
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        (cell as! ExpandTableViewCell).watchFrameChanges()
+    }
+    
+    func tableView(tableView: UITableView, didEndDisplayingCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        (cell as! ExpandTableViewCell).ignoreFrameChanges()
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath == selectedIndexPath {
+            return getHeight(indexPath.row)
+        } else {
+            return self.defaultHeight
+        }
+    }
+    
+    
+    func getHeight(index : Int) -> CGFloat{
+        let testLabel = UILabel(frame: CGRect(x: 0, y: 0, width: susInfoTableView.frame.width, height: 0))
+        testLabel.text = susInfo[index]
+        testLabel.frame.size.width = susInfoTableView.frame.width
+        testLabel.sizeToFit()
+        return testLabel.frame.height
     }
     
     
